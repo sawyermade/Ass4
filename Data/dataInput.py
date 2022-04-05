be477a05338953a21252923eea812fe4b8811e52
sawyermade <sawyermade0@gmail.com>
2022-04-05 00:52:59 -0400

    first

--- Data/dataInput.py
+++ Data/dataInput.py
@@ -3,8 +3,7 @@ import sys
 def getData(data_list):
 	data_list.clear()
 	# [data_list.append(int(i)) for i in sys.argv[1:]]
-	for i in sys.argv[1:]:
-		data_list.append(int(i))
+	for i in sys.argv[1:]: data_list.append(int(i))
 
 def main():
 	data_list = []
